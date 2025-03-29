c31a2ee67eb6ce23b4cdfa6cdb70be5782a382ef
afkzoro <samueltheophilus019@gmail.com>
2025-03-29 05:47:16 +0100

    fix

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,21 +1,73 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
 import * as cloudflare from "@pulumi/cloudflare";
+import * as pulumi from "@pulumi/pulumi";
 
-// Import specific infrastructure modules
-import { createS3Bucket } from "./s3";
-import { setupCloudflare } from "./cloudflare";
+const websiteBucket = new aws.s3.BucketV2("websiteBucket", {
+    bucket: "afkzoro-hugo-site",
+    forceDestroy: false,
+    objectLockEnabled: false,
+});
 
-// Create infrastructure stack
-const config = new pulumi.Config();
-const env = config.require("env"); // 'dev' or 'prod'
+const websiteBucketConfiguration = new aws.s3.BucketWebsiteConfigurationV2("websiteBucketConfiguration", {
+    bucket: websiteBucket.id,
+    indexDocument: {
+        suffix: "index.html",
+    },
+    errorDocument: {
+        key: "error.html",
+    },
+});
 
-// S3 Bucket for static hosting
-const siteBucket = createS3Bucket(env);
+const websiteBucketOwnershipControls = new aws.s3.BucketOwnershipControls("websiteBucketOwnershipControls", {
+    bucket: websiteBucket.id,
+    rule: {
+        objectOwnership: "BucketOwnerPreferred",
+    },
+});
+const websiteBucketPublicAccessBlock = new aws.s3.BucketPublicAccessBlock("website", {
+    bucket: websiteBucket.id,
+    blockPublicAcls: false,
+    blockPublicPolicy: false,
+    ignorePublicAcls: false,
+    restrictPublicBuckets: false,
+});
 
-// Cloudflare DNS and CDN
-const cloudflareSetup = setupCloudflare(siteBucket);
+const websiteBucketAclV2 = new aws.s3.BucketAclV2("website", {
+    bucket: websiteBucket.id,
+    acl: "public-read",
+}, {
+    dependsOn: [
+        websiteBucketOwnershipControls,
+        websiteBucketPublicAccessBlock
+    ],
+});
 
-// Export important outputs
-export const bucketName = siteBucket.id;
-export const websiteEndpoint = siteBucket.websiteEndpoint;
-export const cloudflareDomain = cloudflareSetup.domain;
+const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
+    bucket: websiteBucket.id,
+    policy: websiteBucket.id.apply(id => JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [{
+            Effect: "Allow",
+            Principal: "*",
+            Action: [
+                "s3:GetObject"
+            ],
+            Resource: [
+                `arn:aws:s3:::${id}/*`
+            ]
+        }]
+    }))
+});
+
+const domain = "afkprojects.online";
+const zoneId = "01e4285a6fee1d0469c4536734cf93f5";
+
+const cloudflareRecord = new cloudflare.Record("cloudflareRecord", {
+    zoneId,
+    name: domain,
+    type: "CNAME",
+    value: websiteBucketConfiguration.websiteEndpoint.apply(endpoint => endpoint),
+});
+
+export const bucketWebsiteUrl = websiteBucketConfiguration.websiteEndpoint;
+export const cloudflareDomain = cloudflareRecord.name;
